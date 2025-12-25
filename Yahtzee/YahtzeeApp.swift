f6cf3238d57531ccae098481ecffc65b76d56075
Matthew Parker <marineparker2003@yahoo.com>
2025-12-24 18:23:43 -0600

    Initial Commit

---
+++ Yahtzee/YahtzeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YahtzeeApp.swift
+//  Yahtzee
+//
+//  Created by Matthew Parker on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct YahtzeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
